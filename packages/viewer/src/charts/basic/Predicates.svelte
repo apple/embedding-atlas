67e44d01453311df96c8c8be3679bcccb035553b
Donghao Ren <donghao.ren@gmail.com>
2025-12-03 09:19:59 -0800

    Remove to selection

--- packages/viewer/src/charts/basic/Predicates.svelte
+++ packages/viewer/src/charts/basic/Predicates.svelte
@@ -17,7 +17,7 @@
   import type { PredicatesSpec } from "./types.js";
 
   interface State {
-    selectedPredicates?: string[];
+    selection?: string[];
   }
 
   interface Item {
@@ -38,7 +38,7 @@
   let { colorScheme } = context;
 
   let items = $derived(spec.items ?? []);
-  let selectedPredicates = $derived(chartState.selectedPredicates ?? []);
+  let selection = $derived(chartState.selection ?? []);
 
   let editingEnabled = $state(false);
   let editingItem = $state.raw<Item | null>(null);
@@ -57,9 +57,7 @@
       items: newItems,
     });
     onStateChange({
-      selectedPredicates: (chartState.selectedPredicates ?? []).filter(
-        (x) => newItems.find((item) => item.predicate == x) != null,
-      ),
+      selection: (chartState.selection ?? []).filter((x) => newItems.find((item) => item.predicate == x) != null),
     });
   }
 
@@ -92,13 +90,13 @@
 
     let source = {
       reset: () => {
-        onStateChange({ selectedPredicates: [] });
+        onStateChange({ selection: [] });
       },
     };
 
     // Sync selection predicate.
     $effect(() => {
-      if (selectedPredicates.length == 0) {
+      if (selection.length == 0) {
         context.filter.update({
           source: source,
           clients: new Set<MosaicClient>([client]),
@@ -106,7 +104,7 @@
           value: null,
         });
       } else {
-        let preds = selectedPredicates.map((x) => "(" + x + ")").join(" OR ");
+        let preds = selection.map((x) => "(" + x + ")").join(" OR ");
         context.filter.update({
           source: source,
           clients: new Set<MosaicClient>([client]),
@@ -131,7 +129,7 @@
 <Container width={width} height={height} scrollY={true}>
   <div class="flex flex-col gap-1">
     {#each items as item}
-      {@const isSelected = selectedPredicates.indexOf(item.predicate) >= 0}
+      {@const isSelected = selection.indexOf(item.predicate) >= 0}
       <div
         class="flex gap-4 w-full bg-white dark:bg-slate-800 p-2 rounded-md border border-slate-200 dark:border-slate-600 select-none"
         class:!bg-blue-100={isSelected}
@@ -144,15 +142,13 @@
           onclick={(e) => {
             if (e.shiftKey) {
               onStateChange({
-                selectedPredicates: isSelected
-                  ? selectedPredicates.filter((x) => x != item.predicate)
-                  : [...selectedPredicates, item.predicate],
+                selection: isSelected ? selection.filter((x) => x != item.predicate) : [...selection, item.predicate],
               });
             } else {
               if (isSelected) {
-                onStateChange({ selectedPredicates: [] });
+                onStateChange({ selection: [] });
               } else {
-                onStateChange({ selectedPredicates: [item.predicate] });
+                onStateChange({ selection: [item.predicate] });
               }
             }
           }}
